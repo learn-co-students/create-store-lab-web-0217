78a05cac5bbbef805a606c90c38cb1a37326433c
hwalborn <hwalborn@gmail.com>
2017-04-12 14:46:14 -0400

    username and hometowns all around

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,9 @@
 export default function manageUsers(state, action){
+  switch (action.type) {
+    case "ADD_USER":
+      let addedUser = [...state.users, action.user]
+      return {users: addedUser}
+    default:
+      return state
+  }
 }
