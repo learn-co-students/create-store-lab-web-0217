1e3bee747650da47def96fdd1212a9837ea25b4e
fakebenjay <fakebenjay@gmail.com>
2017-04-13 17:20:11 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,14 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+  users: []
+}, action) {
+  console.log(action)
+  switch(action.type) {
+
+    case 'ADD_USER':
+      let user = { username: action.user.username, hometown: action.user.hometown }
+      return Object.assign({}, state, { users: state.users.concat(user) });
+
+    default:
+      return state;
+  }
 }
