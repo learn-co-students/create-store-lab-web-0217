17d62c32ba725d8c52397226270b81d4bdfa44b5
gee2789 <geeyung.li@gmail.com>
2017-04-13 18:09:40 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,18 @@
-export default function manageUsers(state, action){
-}
+
+
+export default function manageUsers(state = {
+  users: []}, action) {
+
+  switch(action.type){
+      case 'ADD_USER':
+        state = {users: [{}, {}]}
+        const user = {users: [{username: action.user.username, hometown: action.user.hometown}]}
+        debugger
+        return Object.assign({}, state, user)
+
+      default:
+      return state
+  }
+
+
+  }
