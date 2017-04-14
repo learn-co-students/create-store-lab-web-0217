d75d3534d5509877a6f24cb492dcb322ee7782a7
“aroncey” <ssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-04-14 17:16:49 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,14 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+  users: []
+}, action){
+
+  switch (action.type) {
+    case 'ADD_USER':
+    debugger
+        console.log(action.users)
+        return Object.assign({}, state, { users: state.users.concat(action.users) });
+    default:
+      return state;
+  }
+
 }
