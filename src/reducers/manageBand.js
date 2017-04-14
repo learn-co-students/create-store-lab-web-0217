51337e1e263da5b59b9784f21009a83247fb1ac2
CorinneKelly <corinnemariekelly@gmail.com>
2017-04-14 14:46:03 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,12 @@
-export default function manageUsers(state, action){
+
+export default function manageUsers(state={users: []}, action){
+	
+	switch(action.type) {
+		case 'ADD_USER':
+			let newState = {...state}
+			newState.users.push(action.user)
+			return newState
+		default:
+			return state
+	}
 }
